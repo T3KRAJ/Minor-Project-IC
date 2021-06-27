835b6fd479ebbe84d13d0461278c476bed049a72
Tek Raj Joshi <tekrajjoshi07@gmail.com>
2021-06-27 22:09:13 +0530

    models for image created

--- server/core/admin.py
+++ server/core/admin.py
@@ -1,3 +1,5 @@
+from core.models import Image
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Image)
--- server/core/models.py
+++ server/core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    picture = models.ImageField()
+    result = models.CharField(max_length=100, blank=True)
+    uploaded_on = models.DateTimeField(auto_now_add=True)
+
