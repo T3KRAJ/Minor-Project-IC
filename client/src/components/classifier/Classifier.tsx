447bcf5c07ca5cf791f61f75870ed01e9e01ea96
GaneshPandey-GP <ganeshpandey897@gmail.com>
2021-07-03 20:40:45 +0530

    update style

--- client/src/components/classifier/Classifier.tsx
+++ client/src/components/classifier/Classifier.tsx
@@ -1,29 +1,43 @@
 import React,{useState} from 'react'
 import Dropzone from 'react-dropzone'
-import Button from '@material-ui/core/Button';
+import {BsFillImageFill} from "react-icons/bs"
+import './Classifier.css';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Container from '@material-ui/core/Container';
+
 
 const Classifier:React.FC = () => {
     const [files,setFiles]=useState([{"name":"","size":""}])
+    const[loading,setLoading]=useState(false)
     
     const onDrop = (acceptedFiles:any)  =>{
-      setFiles(acceptedFiles)
+      setLoading(true)
+      Loadimage(acceptedFiles)
     }
+   const Loadimage=(acceptedFiles:any)=>{
+     setInterval(()=>{
+      setFiles(acceptedFiles)
+      setLoading(false)
+     },1000)
+   
+   }
     const file = files.map(file => (
         <li key={file.name}>
           {file.name} - {file.size} bytes
         </li>
         ))
-
+  console.log(files)
     return (
-        <Dropzone onDrop={acceptedFiles => onDrop(acceptedFiles)}>
-        {({getRootProps, getInputProps}) => (
-          <section className="block-example border border-dark" style={{padding:`12px`,border:`3px dotted gray`,marginTop:'25px' }}>
-            <div {...getRootProps({className: 'dropzone'})}>
+        <Dropzone onDrop={acceptedFiles => onDrop(acceptedFiles)} >
+        {({isDragActive,getRootProps, getInputProps}) => (
+          <Container maxWidth="sm">
+            <div {...getRootProps({className: 'dropzone main'})}>
               <input {...getInputProps()} />
-              <p>Drag 'n' drop some files here, or click to select files</p>
+              <BsFillImageFill size="40px" color="#332f2f"/>
+              <p>{isDragActive? "Drop Some images":  "Drag 'n' drop some files here, or click to select files"}</p>
             </div>
-            <p>{file}</p>
-          </section>
+            {loading?<CircularProgress color="primary" />: <ul>{file}</ul>}
+          </Container>
         )}
       </Dropzone>
     )
