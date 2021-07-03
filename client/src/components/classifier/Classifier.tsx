6746d541e5f2f404a7e2bd0e088dc6f65e4438b9
GaneshPandey-GP <ganeshpandey897@gmail.com>
2021-07-03 17:05:56 +0530

    add react material-UI

--- client/src/components/classifier/Classifier.tsx
+++ client/src/components/classifier/Classifier.tsx
@@ -1,5 +1,6 @@
 import React,{useState} from 'react'
 import Dropzone from 'react-dropzone'
+import Button from '@material-ui/core/Button';
 
 const Classifier:React.FC = () => {
     const [files,setFiles]=useState([{"name":"","size":""}])
@@ -16,8 +17,8 @@ const Classifier:React.FC = () => {
     return (
         <Dropzone onDrop={acceptedFiles => onDrop(acceptedFiles)}>
         {({getRootProps, getInputProps}) => (
-          <section>
-            <div {...getRootProps()}>
+          <section className="block-example border border-dark" style={{padding:`12px`,border:`3px dotted gray`,marginTop:'25px' }}>
+            <div {...getRootProps({className: 'dropzone'})}>
               <input {...getInputProps()} />
               <p>Drag 'n' drop some files here, or click to select files</p>
             </div>
